2caa846d1308527214820201685aa873ab6f6c99
vboxuser <vboxuser@Ubuntu-18.myguest.virtualbox.org>
2024-07-05 08:43:28 +0530

    Added a for loop

---
+++ basic-for-loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash   
+
+# For loop demo
+
+# Syntax
+# for <VARIABLE> in {first_num..end_num}
+# do
+#     YOUR STATEMENT
+# done
+
+for num in {1..5}
+do
+	echo "Manoj, Number is : $num"
+done
